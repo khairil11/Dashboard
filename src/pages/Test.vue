d659b3b589176488c8f0cce80da66ec2f1cb1f33
Qibert <ilyassmsdn@gmail.com>
2021-02-07 21:04:32 +0700

    innitial commit

---
+++ src/pages/Test.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+        <b-row>
+      <b-col md="6" xl="3" sm="6" xs="12">
+        <div class="pb-xlg h-100">
+          <Widget class="h-100 mb-0" title="Visits Today" close>
+            <div class="d-flex justify-content-between align-items-center mb-lg">
+              <h2>4,332</h2>
+              <i class="la la-arrow-right text-primary la-lg rotate-315" />
+            </div>
+            <div class="d-flex flex-wrap justify-content-between">
+              <div class="mt">
+                <h6>+830</h6><p class="text-muted mb-0 mr"><small>Logins</small></p>
+              </div>
+              <div class="mt">
+                <h6>0.5%</h6><p class="text-muted mb-0"><small>Sign Out</small></p>
+              </div>
+              <div class="mt">
+                <h6>4.5%</h6><p class="text-muted mb-0 mr"><small>Rate</small></p>
+              </div>
+            </div>
+          </Widget>
+        </div>
+      </b-col>
+        </b-row>
+        
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
